e533c8220fbeae8e8b5936ffa603facd0ede648e
Cameron <camdunne@gmail.com>
2017-07-15 16:41:06 -0400

    fix(merge): client

--- client/screens/Splash.jsx
+++ client/screens/Splash.jsx
@@ -18,11 +18,7 @@ class Splash extends Component {
         <br />
         <Link to="/allposts">{'all postings'}</Link>
         <div>{'Hello user: '}{this.props.email}</div>
-<<<<<<< HEAD
-        <a href="/api/users/logout">{'log out'}</a>
-=======
         <a href="/logout">{'log out'}</a>
->>>>>>> mergeejs
       </div>
     );
   }
